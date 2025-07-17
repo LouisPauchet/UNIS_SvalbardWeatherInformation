30dd664c4e8cc016af2fa30fc1c49ae5452ce891
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-07-18 00:56:43 +0200

    add config

--- frontend/src/maps/geocoding.js
+++ frontend/src/maps/geocoding.js
@@ -1,6 +1,11 @@
 import "../css/geocoding.css";
 import L from "leaflet";
 
+/**
+ * Fetches place information from the Norwegian Polar Institute API based on the provided ID.
+ * @param {string} id - The ID of the place to fetch information for.
+ * @returns {Promise<Object>} - A promise that resolves to the place information data.
+ */
 async function fetchPlaceInformation(id) {
   const response = await fetch(
     `https://v2-api.npolar.no/placename/feature/${id}`
@@ -9,6 +14,12 @@ async function fetchPlaceInformation(id) {
   return data;
 }
 
+/**
+ * Displays place information on the map and in the information div.
+ * @param {string} name - The name of the place.
+ * @param {string} id - The ID of the place.
+ * @param {Object} map - The Leaflet map object.
+ */
 async function displayPlaceInformation(name, id, map) {
   const placeInformationDiv = document.getElementById(
     "geocoding-place-information"
@@ -75,6 +86,12 @@ async function displayPlaceInformation(name, id, map) {
 }
 
 // searchHandler.js
+
+/**
+ * Fetches place name suggestions from the Norwegian Polar Institute API based on the provided query.
+ * @param {string} query - The search query for place names.
+ * @returns {Promise<Array<Object>>} - A promise that resolves to an array of place name suggestions.
+ */
 export async function fetchSuggestions(query) {
   const response = await fetch(
     `https://api.npolar.no/placename/?q-name.@value=${query}&filter-area=Svalbard&filter-status=official&not-terrain.nn=utmål|verneområde&format=geojson&fields=id,type,geometry,name,area,terrain,terrain_type&limit=10`
@@ -86,6 +103,13 @@ export async function fetchSuggestions(query) {
   }));
 }
 
+/**
+ * Displays suggestions in the suggestions container and handles user interactions.
+ * @param {Array<Object>} suggestions - An array of suggestion objects.
+ * @param {HTMLElement} suggestionsContainer - The container element to display suggestions in.
+ * @param {HTMLElement} searchInput - The search input element.
+ * @param {Object} map - The Leaflet map object.
+ */
 export function displaySuggestions(
   suggestions,
   suggestionsContainer,
@@ -94,6 +118,7 @@ export function displaySuggestions(
 ) {
   suggestionsContainer.innerHTML = "";
   let selectedSuggestionIndex = -1; // No suggestion selected initially
+
   suggestions.forEach((suggestion, index) => {
     const div = document.createElement("div");
     div.textContent = suggestion.name;
@@ -143,6 +168,11 @@ export function displaySuggestions(
 
   suggestionsContainer.style.display = "block";
 
+  /**
+   * Updates the highlight of suggestions based on the selected index.
+   * @param {NodeList} suggestions - The list of suggestion elements.
+   * @param {number} selectedIndex - The index of the selected suggestion.
+   */
   function updateSuggestionsHighlight(suggestions, selectedIndex) {
     suggestions.forEach((suggestion, index) => {
       if (index === selectedIndex) {
@@ -154,6 +184,12 @@ export function displaySuggestions(
   }
 }
 
+/**
+ * Initializes the geocoding search bar with event listeners for input and suggestion display.
+ * @param {string} inputId - The ID of the search input element.
+ * @param {string} suggestionsContainerId - The ID of the suggestions container element.
+ * @param {Object} map - The Leaflet map object.
+ */
 export function initializeGeocodingSearchBar(
   inputId,
   suggestionsContainerId,
