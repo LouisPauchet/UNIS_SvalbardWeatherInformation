b8f96d7004b01b508a770aa385f2db4968b2216c
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-07-31 01:17:55 +0200

    update some logging + caching container

--- source/datasource/datasourceFactory.py
+++ source/datasource/datasourceFactory.py
@@ -30,7 +30,7 @@ def get_datasource(station_id, config=None):
     logger = Logger.setup_logger("get_datasource")
     config = ConfigHandler()
 
-    logger.info(f"Fetching metadata for station_id: {station_id}")
+    logger.debug(f"Fetching metadata for station_id: {station_id}")
     station = config.get_metadata(station_id)
     #print(station)
     # Get the datasource name from metadata
@@ -41,20 +41,23 @@ def get_datasource(station_id, config=None):
     if source_name not in DATASOURCE_MAPPING:
         #print(type)
         if type == 'mobile':
-            logger.warning(f"Unknown datasource '{source_name}' for station_id {station_id}, defaulting to FrostBoatSource.")
+            logger.info(f"Unknown datasource '{source_name}' for station_id {station_id}, defaulting to FrostBoatSource.")
             source_name = "FrostBoatSource"
         else:
-            logger.warning(f"Unknown datasource '{source_name}' for station_id {station_id}, defaulting to FrostSource.")
+            logger.info(f"Unknown datasource '{source_name}' for station_id {station_id}, defaulting to FrostSource.")
             source_name = "FrostSource"
         
 
-    logger.info(f"Datasource identified for {station_id}: {source_name}")
+    logger.debug(f"Datasource identified for {station_id}: {source_name}")
 
     # Get the actual class from the mapping dictionary
     source_class = DATASOURCE_MAPPING[source_name]
 
-    logger.info(f"Fetching API Key for: {source_name}")
+    logger.debug(f"Fetching API Key for: {source_name}")
     # Get API credentials for this datasource
     api_key = config.get_api_credential(source_name)
+
+    logger.debug(f"Key to be passed : {api_key}")
+
 
     return source_class(api_key=api_key)
